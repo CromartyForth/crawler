c415b24c474bc654312b70664bb7d1acfa25a2c4
CromartyForth <glenn@gcampbell.co.uk>
2026-05-21 23:59:18 +0100

    added get urls from html tests

---
+++ get_urls_From_html.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	// "github.com/PuerkitoBio/goquery"
+)
+
+func getURLsFromHTML(input string, aseURL *url.URL ) (string, error) {
+
+	return fmt.Sprintln("hello world"), nil
+}
