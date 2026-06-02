994627ab9bc1943812bf9fd076e1a7a90553b261
CromartyForth <glenn@gcampbell.co.uk>
2026-06-02 23:13:02 +0100

    added command line arguments to main

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 	
 import (
+	"os"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	if len(os.Args) < 2{
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} 
+	baseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %v\n",baseURL)
+	os.Exit(0)
 }
